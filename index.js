1f7ed4d3944699e2f50e560f71d8d669e9311fbf
Ingvar Stepanyan <me@rreverser.com>
2014-07-15 20:48:25 +0300

    Added comments.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 
 var code = fs.readFileSync('test.js', 'utf-8');
 
+// parsing original code, preserving locations and linkage to original file
 var ast = esprima.parse(code, {
 	loc: true,
 	source: 'test.js'
@@ -12,15 +13,20 @@ var ast = esprima.parse(code, {
 
 ast = estraverse.replace(ast, {
 	leave: function (node) {
+		// Looking for arrow function expressions i.e. (arg1, arg2) => arg1 * arg2
 		if (node.type === 'ArrowFunctionExpression') {
+			// Replacing them with
 			return {
+				// calls where
 				type: 'CallExpression',
 				callee: {
 					type: 'MemberExpression',
+					// we create simple function expression...
 					object: {
 						type: 'FunctionExpression',
-						id: node.id,
+						// using original function's parameters
 						params: node.params,
+						// and it's expression, wrapped into { return ... } block
 						body: {
 							type: 'BlockStatement',
 							body: [{
@@ -30,20 +36,25 @@ ast = estraverse.replace(ast, {
 						}
 					},
 					computed: false,
+					// ...and .bind(...) it
 					property: {
 						type: 'Identifier',
 						name: 'bind'
 					}
 				},
+				// to current `this` context
 				arguments: [{
 					type: 'ThisExpression'
 				}]
 			};
 		}
 
+		// Looking for any binary operators
 		if (node.type === 'BinaryExpression') {
 			return {
+				// and replacing them with ternary operator where
 				type: 'ConditionalExpression',
+				// condition is `typeof (left argument) === 'object'`
 				test: {
 					type: 'BinaryExpression',
 					operator: '===',
@@ -58,6 +69,7 @@ ast = estraverse.replace(ast, {
 						value: 'object'
 					}
 				},
+				// which results into `left[operator](right)` call if true
 				consequent: {
 					type: 'CallExpression',
 					callee: {
@@ -71,16 +83,19 @@ ast = estraverse.replace(ast, {
 					},
 					arguments: [node.right]
 				},
+				// and original expression if false
 				alternate: node
 			};
 		}
 	}
 });
 
+// generating resulting code
 var output = escodegen.generate(ast, {
 	sourceMap: true,
 	sourceMapWithCode: true
 });
 
+// and writing as code + source map pair
 fs.writeFileSync('test.out.js', output.code);
 fs.writeFileSync('test.out.js.map', output.map.toString());
