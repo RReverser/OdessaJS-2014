0c20b6156e077dd5d09c744506316fb15395fb2a
Ingvar Stepanyan <me@rreverser.com>
2014-07-15 20:35:05 +0300

    Simplified sample by making reading synchronous.

--- index.js
+++ index.js
@@ -3,72 +3,70 @@ var estraverse = require('estraverse');
 var escodegen = require('escodegen');
 var fs = require('fs');
 
-fs.readFile('test.js', 'utf-8', function (err, code) {
-	if (err) throw err;
+var code = fs.readFileSync('test.js', 'utf-8');
 
-	var ast = esprima.parse(code, {
-		loc: true,
-		source: 'test.js'
-	});
+var ast = esprima.parse(code, {
+	loc: true,
+	source: 'test.js'
+});
 
-	ast = estraverse.replace(ast, {
-		leave: function (node) {
-			if (node.type === 'ArrowFunctionExpression') {
-				return {
-					type: 'FunctionExpression',
-					id: node.id,
-					params: node.params,
-					body: {
-						type: 'BlockStatement',
-						body: [{
-							type: 'ReturnStatement',
-							argument: node.body
-						}]
+ast = estraverse.replace(ast, {
+	leave: function (node) {
+		if (node.type === 'ArrowFunctionExpression') {
+			return {
+				type: 'FunctionExpression',
+				id: node.id,
+				params: node.params,
+				body: {
+					type: 'BlockStatement',
+					body: [{
+						type: 'ReturnStatement',
+						argument: node.body
+					}]
+				}
+			};
+		}
+
+		if (node.type === 'BinaryExpression') {
+			return {
+				type: 'ConditionalExpression',
+				test: {
+					type: 'BinaryExpression',
+					operator: '===',
+					left: {
+						type: 'UnaryExpression',
+						operator: 'typeof',
+						argument: node.left,
+						prefix: true
+					},
+					right: {
+						type: 'Literal',
+						value: 'object'
 					}
-				};
-			}
-
-			if (node.type === 'BinaryExpression') {
-				return {
-					type: 'ConditionalExpression',
-					test: {
-						type: 'BinaryExpression',
-						operator: '===',
-						left: {
-							type: 'UnaryExpression',
-							operator: 'typeof',
-							argument: node.left,
-							prefix: true
-						},
-						right: {
+				},
+				consequent: {
+					type: 'CallExpression',
+					callee: {
+						type: 'MemberExpression',
+						computed: true,
+						object: node.left,
+						property: {
 							type: 'Literal',
-							value: 'object'
+							value: node.operator
 						}
 					},
-					consequent: {
-						type: 'CallExpression',
-						callee: {
-							type: 'MemberExpression',
-							computed: true,
-							object: node.left,
-							property: {
-								type: 'Literal',
-								value: node.operator
-							}
-						},
-						arguments: [node.right]
-					},
-					alternate: node
-				};
-			}
+					arguments: [node.right]
+				},
+				alternate: node
+			};
 		}
-	});
-
-	var output = escodegen.generate(ast, {
-		sourceMap: true,
-		sourceMapWithCode: true
-	});
-
-	fs.writeFileSync('test.out.js', output.code);
-	fs.writeFileSync('test.out.js.map', output.map.toString());
+	}
 });
+
+var output = escodegen.generate(ast, {
+	sourceMap: true,
+	sourceMapWithCode: true
+});
+
+fs.writeFileSync('test.out.js', output.code);
+fs.writeFileSync('test.out.js.map', output.map.toString());
